0ba47983ec069769882d6156447ade3637b7df8b
Lukas Gamper <lukas.gamper@usystems.ch>
2023-10-24 10:10:59 +0200

    minor improvements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # orbita
+
+Try it out on https://raw.githack.com/usystems/orbita/main/index.html
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const cacheName = 'v1';
 const cacheFiles = [
-    './',
+    // './',
     './index.html'
     // './css/style.css',
     // './js/script.js'
